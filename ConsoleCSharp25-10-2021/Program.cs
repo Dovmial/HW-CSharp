3da977190e937445db1d6f867e7bd3192f1ab51c
Dovmial <nma91@mail.ru>
2021-10-29 01:16:54 +0500

    creating

---
+++ ConsoleCSharp25-10-2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCSharp25_10_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
