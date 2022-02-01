b404d7d58db7cc75773d1bbbef2ec7173348fe3a
Dovmial <nma91@mail.ru>
2022-02-02 01:04:28 +0500

    add Documentation for CustomStack

--- CustomStack/CustomStack/CustomStack.cs
+++ CustomStack/CustomStack/CustomStack.cs
@@ -5,23 +5,64 @@ using System.Collections.Generic;
 
 namespace CustomStackProject
 {
+    /// <summary>
+    /// Пользовательская реализация стека
+    /// </summary>
+    /// <typeparam name="T">шаблонный тип содержимого стека</typeparam>
     public class CustomStack<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Узел стека
+        /// </summary>
         private class Node
         {
+            /// <summary>
+            /// Хранящиеся в узле данные
+            /// </summary>
             public T Data { get; set; }
+            /// <summary>
+            /// ссылка на правый узел в стеке
+            /// </summary>
             public Node Right { get; set; }
+            /// <summary>
+            /// ссылка на левый узел в стеке
+            /// </summary>
             public Node Left { get; set; }
+
+            /// <summary>
+            /// Конструктор узла
+            /// </summary>
+            /// <param name="data">данные для хранения</param>
             public Node(T data) => Data = data;
         }
 
+        /// <summary>
+        /// перечислитель по стеку
+        /// </summary>
         private class StackEnumerator : IEnumerator<T>
         {
+            /// <summary>
+            /// ссылка на стек
+            /// </summary>
             private CustomStack<T> _stack;
+
+            /// <summary>
+            /// текущее положение указателя на узел
+            /// </summary>
             public Node Cur { get; private set; }
+            /// <summary>
+            /// упаковка узла
+            /// </summary>
             public object Current => Cur;
+            /// <summary>
+            /// Данные в выбранном узле
+            /// </summary>
             T IEnumerator<T>.Current => Cur.Data;
 
+            /// <summary>
+            /// Конструктор перечислителя
+            /// </summary>
+            /// <param name="stack">сслыка на пользовательский стек</param>
             public StackEnumerator(CustomStack<T> stack)
             {
                 _stack = stack;
@@ -29,6 +70,10 @@ namespace CustomStackProject
                 Cur.Left = _stack._head;
             }
 
+            /// <summary>
+            /// Перемещение по стеку
+            /// </summary>
+            /// <returns>возврат возможности переместиться на следующий узел</returns>
             public bool MoveNext()
             {
                 if (Cur.Left == null)
@@ -36,21 +81,46 @@ namespace CustomStackProject
                 Cur = Cur.Left;
                 return true;
             }
-
+            /// <summary>
+            /// неиспользуемая, функция интерфейса 
+            /// </summary>
             public void Reset()
             {}
-
+            /// <summary>
+            /// неиспользуемая, функция интерфейса 
+            /// </summary>
             public void Dispose()
             {}
         }
+        /// <summary>
+        /// вместимость стека
+        /// </summary>
         public int Capacity { get; private set; }
+        /// <summary>
+        /// количество элементов в стеке
+        /// </summary>
         public int Count { get; private set; }
 
+        /// <summary>
+        /// ссылка на голову стека
+        /// </summary>
         private Node _head;
+        /// <summary>
+        /// ссылкаа на хвост стека
+        /// </summary>
         private Node _tail;
        
+        /// <summary>
+        /// конструктор стека
+        /// </summary>
+        /// <param name="capacity">базовая вместимость стека</param>
         public CustomStack(int capacity) => Capacity = capacity;
         
+        /// <summary>
+        /// изъятие элемента с вершины стека
+        /// </summary>
+        /// <returns>T - хранящиеся данные</returns>
+        /// <exception cref="NullReferenceException"></exception>
         public T Take()
         {
             if(Count == 0)
@@ -62,6 +132,11 @@ namespace CustomStackProject
             _head.Right = null;
             return pointer.Data;
         }
+
+        /// <summary>
+        /// положить эелемент в стек
+        /// </summary>
+        /// <param name="data">складируемые данные</param>
         public void Put(T data)
         {
             if(Count == 0) 
@@ -89,7 +164,11 @@ namespace CustomStackProject
             }
                 
         }
-
+        /// <summary>
+        /// посмотреть данные в вершине сека, не удаляя их из стека
+        /// </summary>
+        /// <returns>T - данные в вершине стека</returns>
+        /// <exception cref="NullReferenceException"></exception>
         public T Peek()
         {
             if (Count == 0)
@@ -97,11 +176,19 @@ namespace CustomStackProject
             return _head.Data;
         }
 
+        /// <summary>
+        /// получение перечислителя
+        /// </summary>
+        /// <returns>IEnumerator</returns>
         public IEnumerator<T> GetEnumerator()
         {
             return new StackEnumerator(this);
         }
 
+        /// <summary>
+        /// перечислитель из интерфейса
+        /// </summary>
+        /// <returns>IEnumerator</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
